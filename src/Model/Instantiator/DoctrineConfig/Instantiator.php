9e347a7549c1210d3fd4d27a7eb46169b6465bd5
rmontagu <rmontagu@users.noreply.github.com>
2023-10-30 14:25:12 +0100

    Update Instantiator.php

--- src/Model/Instantiator/DoctrineConfig/Instantiator.php
+++ src/Model/Instantiator/DoctrineConfig/Instantiator.php
@@ -3,13 +3,12 @@
 namespace Biig\Component\Domain\Model\Instantiator\DoctrineConfig;
 
 use Biig\Component\Domain\Event\DomainEventDispatcherInterface;
-use Biig\Component\Domain\Model\Instantiator\Instantiator as BaseInstantiator;
 use Doctrine\Instantiator\InstantiatorInterface;
 
 /**
  * Decorates the Doctrine standard instantiator to add new behavior.
  */
-class Instantiator extends BaseInstantiator implements InstantiatorInterface
+class Instantiator implements InstantiatorInterface
 {
     public function __construct(DomainEventDispatcherInterface $dispatcher)
     {
@@ -22,5 +21,12 @@ class Instantiator extends BaseInstantiator implements InstantiatorInterface
         $this->injectDispatcher($object);
 
         return $object;
+    }
+
+    protected function injectDispatcher($object)
+    {
+        if ($object instanceof ModelInterface) {
+            $object->setDispatcher($this->dispatcher);
+        }
     }
 }
